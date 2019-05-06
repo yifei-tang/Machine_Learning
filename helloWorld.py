7b5c5f6a108db0b212fe85d4b4c65fe73bcefabf
yifei-tang <yifeit.tang@mail.utoronto.ca>
2019-05-06 16:07:34 -0400

    model to predict housing prices

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,10 @@
-import tensorflow
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
 
-print("hello world")
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+ys = np.array([1,1.5,2,2.5,3,3.5,4])
+xs = np.array([1,2,3,4,5,6,7])
+model.fit(xs,ys,epochs=500)
+print(model.predict([7.0]))
